0b63cef549ad008c8a4e170f1929a62831f9bebe
Leomar Scheunemann Junior <255654+leomarsjr@users.noreply.github.com>
2023-11-30 22:17:03 +0100

    respond with 200

--- app/server.go
+++ app/server.go
@@ -13,9 +13,22 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	go func() {
+		bytes := make([]byte, 0)
+		_, err := conn.Read(bytes)
+		if err != nil {
+			fmt.Println("Failed to read from connection")
+			os.Exit(1)
+		}
+		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		if err != nil {
+			fmt.Println("Failed to write response")
+			os.Exit(1)
+		}
+	}()
 }
