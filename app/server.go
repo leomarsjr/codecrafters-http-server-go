b8f03f0f93d04cebfe06549b0c55680b1d10b580
Leomar Scheunemann Junior <255654+leomarsjr@users.noreply.github.com>
2023-12-22 18:17:49 +0100

    remove goroutine

--- app/server.go
+++ app/server.go
@@ -18,17 +18,15 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	go func() {
-		bytes := make([]byte, 0)
-		_, err := conn.Read(bytes)
-		if err != nil {
-			fmt.Println("Failed to read from connection")
-			os.Exit(1)
-		}
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		if err != nil {
-			fmt.Println("Failed to write response")
-			os.Exit(1)
-		}
-	}()
+	bytes := make([]byte, 0)
+	_, err = conn.Read(bytes)
+	if err != nil {
+		fmt.Println("Failed to read from connection")
+		os.Exit(1)
+	}
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Failed to write response")
+		os.Exit(1)
+	}
 }
