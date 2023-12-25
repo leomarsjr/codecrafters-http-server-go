f68ae35e4ad6475ec99b8348a539ddf31b993d46
Leomar Scheunemann Junior <255654+leomarsjr@users.noreply.github.com>
2023-12-25 11:09:01 +0100

    add 404 response for non-root path

--- app/server.go
+++ app/server.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+const bufferSize = 4096
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -19,13 +22,45 @@ func main() {
 		os.Exit(1)
 	}
 
-	writeResponse(conn)
+	buffer := make([]byte, bufferSize)
+	readRequest(conn, buffer)
+
+	status := parseInput(buffer)
+
+	writeResponse(conn, status)
 }
 
-func writeResponse(conn net.Conn) {
-	_, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+func parseInput(input []byte) int {
+	path := strings.Fields(string(input))[1]
+	if path != "/" {
+		return 404
+	}
+	return 200
+}
+
+func readRequest(conn net.Conn, buffer []byte) {
+	_, err := conn.Read(buffer)
 	if err != nil {
-		fmt.Println("Failed to write response")
+		fmt.Println("Error reading request: ", err.Error())
 		os.Exit(1)
 	}
+}
+
+func writeResponse(conn net.Conn, status int) {
+	responseHeader := createResponseHeader(status)
+	_, err := conn.Write([]byte(responseHeader))
+	if err != nil {
+		fmt.Println("Failed to write response: ", err.Error())
+		os.Exit(1)
+	}
+}
+
+func createResponseHeader(status int) string {
+	switch status {
+	case 200:
+		return "HTTP/1.1 200 OK\r\n\r\n"
+	case 404:
+		return "HTTP/1.1 404 NOT FOUND\r\n\r\n"
+	}
+	return ""
 }
